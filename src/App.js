0bd30870fe6bc9196841cf0a1aded3cfe71e406d
Lauren Buchholz <90865074+L-Buchholz@users.noreply.github.com>
2022-01-29 18:33:06 -0700

    More additions
    Building up the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./App.css";
 import NavBar from "./components/Navbar";
 import AboutMe from "./components/AboutMe";
 //import Portfolio from ./components/Portfolio";
 //import Resume from ./components/Resume";
 //import ContactMe from ./components/ContactMe";
-import Footer from ".components/Footer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,39 +5,41 @@ import "../Card.css";
 function Portfolio() {
   return (
     //Does this need to fall under "body"?
-    <article>
-      <div>
-        <h2 id="my-work">My Work</h2>
-      </div>
-          <div class="example1">
-            <a href="https://l-buchholz.github.io/project-1/" target="blank">
-              <img
-                id="top"
-                src="./assets/images/uProjeckt-screenshot.jpg"
-                alt="uProjekt webpage project management tool for programmers (top project)"
-              />
-            </a>
-          </div>
-        </article>
-        <article>
-          <div class="blankspace"></div>
-          <div class="examples github-link" id="examples1">
-            <a href="https://l-buchholz.github.io/code-quiz/" target="blank">
-              <img
-                src="./assets/images/nursery-rhymes-quiz.png"
-                alt="Interactive timed nursery rhymes quiz (&copy;Lauren Buchholz)"
-              />
-            </a>
-          </div>
-          <div class="examples github-link" id="examples2">
-            <a href="https://l-buchholz.github.io/day-calendar/" target="blank">
-              <img
-                src="./assets/images/workday-calendar.png"
-                alt="Interactive workday scheduler (&copy;Lauren Buchholz)"
-              />
-            </a>
-          </div>
-        </article>
+    <>
+      <article>
+        <div>
+          <h2 id="my-work">My Work</h2>
+        </div>
+        <div class="example1">
+          <a href="https://l-buchholz.github.io/project-1/" target="blank">
+            <img
+              id="top"
+              src="./assets/images/uProjeckt-screenshot.jpg"
+              alt="uProjekt webpage project management tool for programmers (top project)"
+            />
+          </a>
+        </div>
+      </article>
+      <article>
+        <div class="blankspace"></div>
+        <div class="examples github-link" id="examples1">
+          <a href="https://l-buchholz.github.io/code-quiz/" target="blank">
+            <img
+              src="./assets/images/nursery-rhymes-quiz.png"
+              alt="Interactive timed nursery rhymes quiz (&copy;Lauren Buchholz)"
+            />
+          </a>
+        </div>
+        <div class="examples github-link" id="examples2">
+          <a href="https://l-buchholz.github.io/day-calendar/" target="blank">
+            <img
+              src="./assets/images/workday-calendar.png"
+              alt="Interactive workday scheduler (&copy;Lauren Buchholz)"
+            />
+          </a>
+        </div>
+      </article>
+    </>
   );
 }
 
