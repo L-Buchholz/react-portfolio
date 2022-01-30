e98ee0fd15a08133d25c49f05a5bf55f5dda1379
Lauren Buchholz <90865074+L-Buchholz@users.noreply.github.com>
2022-01-30 16:18:43 -0700

    Updated portfolio
    Added six project examples

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -34,16 +34,37 @@ function Portfolio() {
       <div style={styles.article}>
         <h2 id="portfolio">Portfolio</h2>
       </div>
+      <div style={styles.example} id="mileHi">
+        <a href="https://project2-milehi.herokuapp.com/" target="blank">
+          <img
+            style={styles.exampleImg}
+            src={process.env.PUBLIC_URL + "/images/milehi-screenshot.png"}
+            alt="MileHi online database for connecting with Denver communities"
+          />
+        </a>
+      </div>
       <div style={styles.example} id="uProjekt">
         <a href="https://l-buchholz.github.io/project-1/" target="blank">
           <img
             style={styles.exampleImg}
             src={process.env.PUBLIC_URL + "/images/uProjeckt-screenshot.png"}
-            alt="uProjekt webpage project management tool for programmers (top project)"
+            alt="uProjekt webpage project management tool for programmers"
           />
         </a>
       </div>
-      <div style={styles.example} className="examples" id="nurseryRhymesQuiz">
+      <div style={styles.example} id="codeCompanion">
+        <a href="https://github.com/SmithCray/Code-Companion" target="blank">
+          <img
+            style={styles.exampleImg}
+            src={
+              process.env.PUBLIC_URL +
+              "/images/code-companion-wireframe-screenshot.png"
+            }
+            alt="Code Companion online programmer community"
+          />
+        </a>
+      </div>
+      <div style={styles.example} id="nurseryRhymesQuiz">
         <a href="https://l-buchholz.github.io/code-quiz/" target="blank">
           <img
             style={styles.exampleImg}
@@ -52,7 +73,22 @@ function Portfolio() {
           />
         </a>
       </div>
-      <div style={styles.example} className="examples" id="dayCalendar">
+      <div style={styles.example} id="emailRegex">
+        <a
+          href="https://gist.github.com/L-Buchholz/91c98d4b070cced3286b268eb67d5dfd
+"
+          target="blank"
+        >
+          <img
+            style={styles.exampleImg}
+            src={
+              process.env.PUBLIC_URL + "/images/email-regex-gist-screenshot.png"
+            }
+            alt="Email Regex gist on GitHub (&copy;Lauren Buchholz)"
+          />
+        </a>
+      </div>
+      <div style={styles.example} id="dayCalendar">
         <a href="https://l-buchholz.github.io/day-calendar/" target="blank">
           <img
             style={styles.exampleImg}
